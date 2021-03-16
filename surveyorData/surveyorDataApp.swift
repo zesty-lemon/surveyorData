27aa9406220f5e81f70a5a2f0bb0c11406105ffd
zesty-lemon <51659929+zesty-lemon@users.noreply.github.com>
2021-03-16 17:51:23 -0400

    Initial Commit

---
+++ surveyorData/surveyorDataApp.swift
@@ -0,0 +1,20 @@
+//
+//  surveyorDataApp.swift
+//  surveyorData
+//
+//  Created by Giles Lemmon on 3/16/21.
+//
+
+import SwiftUI
+
+@main
+struct surveyorDataApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
